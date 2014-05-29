c7961c7b6253d83dfb6f29f0a452bbee42157d25
Bryan O'Sullivan <bos@serpentine.com>
2014-05-28 23:09:47 -0700

    Getting further

--- exe/PackRank.hs
+++ exe/PackRank.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE BangPatterns, ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns, ScopedTypeVariables, TupleSections #-}
 
 module Main (main) where
 
 import Control.Applicative ((<$>))
 import Control.Exception (try)
+import Control.Monad (forM_)
 import Data.Binary (decodeFileOrFail, encodeFile)
 import Data.Char (isSpace)
 import Data.List (stripPrefix)
 import Data.Maybe (catMaybes)
-import Distribution.PackDeps.Lens
+import Distribution.PackDeps
 import Distribution.PackRank
 import System.Directory (getAppUserDataDirectory, getModificationTime)
+import System.Environment (getArgs)
 import System.FilePath ((</>))
 import System.IO (hPutStrLn, stderr)
 import qualified Data.Map as Map
@@ -20,8 +22,18 @@ import qualified Data.Vector.Unboxed as U
 
 main :: IO ()
 main = do
-  (idx,s) <- getRanks
-  dump idx (prVector s)
+  (mn,idx,s) <- getRanks
+  args <- getArgs
+  case args of
+    [] -> dump idx (prVector s)
+    _  -> do
+      n <- maybe (status "loading newest" >> loadNewest) return mn
+      let r = getReverses n
+      forM_ args $ \a ->
+        print (transitiveReverses a r)
+
+transitiveReverses :: String -> Reverses -> [String]
+transitiveReverses name = maybe [] (map fst . snd) . Map.lookup name
 
 dump :: Index -> U.Vector Double -> IO ()
 dump idx s = do
@@ -32,30 +44,31 @@ dump idx s = do
   G.forM_ tt $ \(r,i) ->
     putStrLn . unwords $ [reverseIdx idx G.! i, show (round (r*k)::Int)]
 
-getRanks :: IO (Index, PackRank)
+getRanks :: IO (Maybe Newest, Index, PackRank)
 getRanks = maybe computeRanks return =<< loadRanks
 
-loadRanks :: IO (Maybe (Index, PackRank))
+loadRanks :: IO (Maybe (Maybe Newest, Index, PackRank))
 loadRanks = do
   it <- minimum <$> (mapM getModificationTime =<< findIndices)
   rp <- rankPath
   rt <- either (\(_::IOError) -> it) id <$> try (getModificationTime rp)
   if rt <= it
     then return Nothing
-    else either (const Nothing) Just <$> decodeFileOrFail rp
+    else either (const Nothing) (\(idx,s) -> Just (Nothing,idx,s)) <$>
+         decodeFileOrFail rp
 
-computeRanks :: IO (Index, PackRank)
+computeRanks :: IO (Maybe Newest, Index, PackRank)
 computeRanks = do
-  status "loading"
+  status "loading newest"
   n <- loadNewest
   status $ show (Map.size n) ++ " packages"
   let idx = makeIndex n
       dep = depending idx n
   status "ranking"
-  let !s = packRank dep defaultAlpha defaultEpsilon
+  let s = packRank dep defaultAlpha defaultEpsilon
   rp <- rankPath
   encodeFile rp (idx, s)
-  return (idx, s)
+  return (Just n, idx, s)
 
 status :: String -> IO ()
 status = hPutStrLn stderr
