c9af7496d719de080bc3e3725e3537f9baaba985
RyanGlScott <ryan.gl.scott@ku.edu>
2014-10-29 16:05:17 -0500

    Fix for lens-4.5

--- lib/Distribution/PackDeps/Lens/Machinery.hs
+++ lib/Distribution/PackDeps/Lens/Machinery.hs
@@ -4,8 +4,8 @@ import Control.Lens ((&), (.~))
 import Control.Lens.TH (DefName(..), lensField, lensRules, makeLensesWith)
 import Language.Haskell.TH.Syntax (Dec, Name, Q, mkName, nameBase)
 
-fieldName :: (String -> String) -> [Name] -> Name -> [DefName]
-fieldName f _ name = [TopName . mkName . f . nameBase $ name]
+fieldName :: (String -> String) -> Name -> [Name] -> Name -> [DefName]
+fieldName f _ _ name = [TopName . mkName . f . nameBase $ name]
 
 makeLenses :: Name -> Q [Dec]
 makeLenses = makeLensesWith (lensRules & lensField .~ fieldName id)
